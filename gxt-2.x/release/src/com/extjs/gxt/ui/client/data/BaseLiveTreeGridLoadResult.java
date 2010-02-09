13ca22e93df6635bb14bc7c43f31e8e88cf853f9
Schmeeky <Schmeeky@c8b75533-744a-0410-987e-e9bf80a15930>
2010-02-09 14:27:53 +0000

    Checked in GXT 2.1.1

--- gxt-2.x/release/src/com/extjs/gxt/ui/client/data/BaseLiveTreeGridLoadResult.java
+++
@@ -1,9 +0,0 @@
-package com.extjs.gxt.ui.client.data;
-
-import java.util.List;
-
-public class BaseLiveTreeGridLoadResult extends BasePagingLoadResult<List<LiveTreeGridModel>> implements
-    LiveTreeGridLoadResult {
-
-  
-}
--- gxt-2.x/release/src/com/extjs/gxt/ui/client/data/LiveTreeGridLoadResult.java
+++
@@ -1,7 +0,0 @@
-package com.extjs.gxt.ui.client.data;
-
-import java.util.List;
-
-public interface LiveTreeGridLoadResult extends PagingLoadResult<List<LiveTreeGridModel>> {
-
-}
