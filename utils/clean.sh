1f89da434a7748f3efb47201f100ca48519b9a5d
Schmeeky <Schmeeky@c8b75533-744a-0410-987e-e9bf80a15930>
2010-04-12 15:49:31 +0000

    Fixed clean script to accept file names with spaces

--- utils/clean.sh
+++ utils/clean.sh
@@ -1,2 +1,7 @@
 #!/bin/sh
-find ./ -type f | grep -v .svn | grep -v clean.sh | grep -v setmime | xargs rm -f
+if test -d "$1"
+then
+  find "$1" -type f -print0 | grep -z -v .svn | grep -z -v clean.sh | grep -z -v setmime | xargs -0 rm -f
+else
+  echo "Please specify a directory to clean"
+fi
